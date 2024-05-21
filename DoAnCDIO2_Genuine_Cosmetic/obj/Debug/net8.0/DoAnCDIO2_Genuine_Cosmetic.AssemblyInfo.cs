e2a6cb4b51c25cc246c604fa582c0de4f27e32f2
Hung <tranquochung4923@gmail.com>
2024-05-22 00:49:58 +0700

    2nd

--- DoAnCDIO2_Genuine_Cosmetic/obj/Debug/net8.0/DoAnCDIO2_Genuine_Cosmetic.AssemblyInfo.cs
+++ DoAnCDIO2_Genuine_Cosmetic/obj/Debug/net8.0/DoAnCDIO2_Genuine_Cosmetic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnCDIO2_Genuine_Cosmetic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3af04453379ba2b6fb0a2797f76bbb9d2acafdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eea1491138413e6b6e99765bcf7002b9d0dc0b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnCDIO2_Genuine_Cosmetic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnCDIO2_Genuine_Cosmetic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
