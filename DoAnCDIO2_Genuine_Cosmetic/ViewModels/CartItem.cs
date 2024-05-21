6df73d21299107b4d2bedcacd2db22091c8f3b34
NTDung1707 <nguyentrungdung1707@gmail.com>
2024-05-21 14:17:33 +0700

    1st commit

---
+++ DoAnCDIO2_Genuine_Cosmetic/ViewModels/CartItem.cs
@@ -0,0 +1,16 @@
+﻿namespace DoAnCDIO2_Genuine_Cosmetic.ViewModels
+{
+    public class CartItem
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public double DonGia { get; set; }
+        public double GiamGia { get; set; }
+        public string Hinh { get; set; }
+        public int soluong { get; set; }
+
+        public double GiaSauGiam => DonGia * (1 - GiamGia / 100.0);
+
+        public double ThanhTien => soluong * GiaSauGiam ; 
+    }
+}
---
+++ DoAnCDIO2_Genuine_Cosmetic/ViewModels/HangHoaVM.cs
@@ -0,0 +1,68 @@
+﻿namespace DoAnCDIO2_Genuine_Cosmetic.ViewModels
+{
+    public class HangHoaVM
+    {
+        public int  MaHh { get; set; }
+        public string TenHh { get; set; }
+        public string HangSx { get; set; }
+        public double DonGia { get; set; }
+        public string Hinh { get; set; }
+        public string TenLoai { get; set; }
+
+        public double GiamGia { get; set; }
+        public double GiaSauGiam => DonGia * (1 - GiamGia / 100.0);
+    }
+    public class ChiTietHangHoaVM
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public string HangSx { get; set; }
+        public double DonGia { get; set; }
+        public string Hinh { get; set; }
+        public string TenLoai { get; set; }
+        public string ChiTiet { get; set; }
+        public int DiemDanhGia { get; set; }
+        public int SoLuongTon { get; set; }
+        public double GiamGia { get; set; }
+        public double GiaSauGiam => DonGia * (1 - GiamGia / 100.0);
+
+    }
+
+    public class ConveyorVM
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public string HangSx { get; set; }
+        public double DonGia { get; set; }
+        public string Hinh { get; set; }
+        public string TenLoai { get; set; }
+        public double GiamGia { get; set; }
+    }
+
+    public class ConveyorVM1
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public string HangSx { get; set; }
+        public double DonGia { get; set; }
+        public string Hinh { get; set; }
+        public string TenLoai { get; set; }
+        public double GiamGia { get; set; }
+    }
+
+    public class SaleVM
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public string HangSx { get; set; }
+        public double DonGia { get; set; }
+        public string Hinh { get; set; }
+        public string TenLoai { get; set; }
+
+        public double GiamGia { get; set; }
+        public double GiaSauGiam => DonGia * (1 - GiamGia / 100.0);
+
+    }
+
+
+}
